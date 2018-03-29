9e074094d25ab65380b1ed47f95aa557b030ed54
pWnH <lukas.schntzmeier@gmail.com>
2018-03-29 22:56:14 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div class="soundButton">
+      <button @click="playSound(sound_1.file)" type="button">{{sound_1.name}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import SoundButton from './components/SoundButton'
+
+export default {
+  name: 'App',
+  components: {
+    SoundButton
+  },
+  data () {
+    return {
+      sound_1: {
+        id: 1,
+        name: 'Bergziege',
+        file: 'C:\\Users\\lukas\\Documents\\Projekte\\wc3soundboard\\src\\bergziege.mp3',
+      },
+    }
+  },
+  methods: {
+    playSound (sound) {
+      let audio = new Audio(sound);
+      setTimeout(() => {
+        audio.play();
+      }, 2000);
+      
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  dipslay: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/SoundButton.vue
@@ -0,0 +1,41 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props:{
+    sound: {
+      required: true,
+    }
+  },
+  methods: {
+    playSound () {
+      let audio = document.getElementById(this.sound.id);
+      audio.play();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
